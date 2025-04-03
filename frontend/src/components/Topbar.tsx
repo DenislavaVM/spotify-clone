4f1817f049cee2354b7bb9a4be0a32b22b201dbf
Deni <denislavavmilanova@outlook.com>
2025-04-03 17:10:13 +0300

    Refactor: Improve Topbar component styling and update logo path

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -1,29 +1,33 @@
-import { SignedIn, SignedOut, SignOutButton } from "@clerk/clerk-react";
-import { LayoutDashboardIcon, Link } from "lucide-react";
+import { SignedOut, UserButton } from "@clerk/clerk-react";
+import { LayoutDashboardIcon } from "lucide-react";
 import SignInOAuthButtons from "./SignInOAuthButtons";
+import { useAuthStore } from "@/stores/useAuthStore";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "./ui/button";
+import { Link } from "react-router-dom";
 
 const Topbar = () => {
-    const isAdmin = false;
+    const { isAdmin } = useAuthStore();
 
     return (
         <div className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 backdrop-blur-md z-10">
             <div className="flex gap-2 items-center">
+                <img src="/spotify-logo.png" className="size-8" alt="spotify-logo" />
                 Spotify
             </div>
             <div className="flex items-center gap-4">
                 {isAdmin && (
-                    <Link to={"/admin"}>
-                        <LayoutDashboardIcon className="size-4 mr-2" />
-                        Admin dashboard</Link>
+                    <Link
+                        to="/admin"
+                        className={cn(buttonVariants({ variant: "outline" }))}>
+                        <LayoutDashboardIcon className="w-4 h-4 mr-2" /> Admin dashboard
+                    </Link>
                 )}
-
-                <SignedIn>
-                    <SignOutButton />
-                </SignedIn>
 
                 <SignedOut>
                     <SignInOAuthButtons />
                 </SignedOut>
+                <UserButton />
             </div>
         </div>
     );
