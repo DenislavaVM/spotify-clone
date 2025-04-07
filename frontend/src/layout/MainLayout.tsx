c5754659223255a04d26bf05ba4ba7c7556d92fa
Deni <denislavavmilanova@outlook.com>
2025-04-07 17:24:07 +0300

    feat(main-layout): add AudioPlayer component to sidebar

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -2,6 +2,7 @@ import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/componen
 import { Outlet } from "react-router-dom";
 import LeftSideBar from "./components/LeftSideBar";
 import FriendsActivity from "./components/FriendsActivity";
+import AudioPlayer from "./components/AudioPlayer";
 
 const MainLayout = () => {
     const isMobile = false;
@@ -10,6 +11,7 @@ const MainLayout = () => {
         <div className="h-screen bg-black text-white flex flex-col">
             <ResizablePanelGroup direction="horizontal" className="flex-1 flex h-full overflow-hidden p-2">
                 <ResizablePanel defaultSize={20} minSize={isMobile ? 0 : 10} maxSize={30}>
+                   <AudioPlayer />
                     <LeftSideBar />
                 </ResizablePanel>
                 <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
