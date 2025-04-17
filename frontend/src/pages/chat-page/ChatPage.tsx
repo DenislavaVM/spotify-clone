6dab0b0cffc2845fac5ec569012ce894ed1dffea
Deni <denislavavmilanova@outlook.com>
2025-04-17 12:32:22 +0300

    feat(chat): integrate ChatPage with user list and real-time socket updates

--- frontend/src/pages/chat-page/ChatPage.tsx
+++ frontend/src/pages/chat-page/ChatPage.tsx
@@ -1,6 +1,32 @@
+import { useChatStore } from "@/stores/useChatStore";
+import { useUser } from "@clerk/clerk-react";
+import { useEffect } from "react";
+import UserList from "./components/UserList";
+import Topbar from "@/components/Topbar";
+
 const ChatPage = () => {
+    const { user } = useUser();
+    const { messages, selectedUser, fetchUsers, fetchMessages } = useChatStore();
+
+    useEffect(() => {
+        if (user) {
+            fetchUsers();
+        };
+    }, [fetchUsers, user]);
+
+    useEffect(() => {
+        if (selectedUser) {
+            fetchMessages(selectedUser.clerkId);
+        };
+    }, [selectedUser, fetchMessages]);
+
     return (
-        <div>ChatPage</div>
+        <main className="h-full rounded-lg bg-gradient-to-b from-zinc-800 to-zinc-900 overflow-hidden">
+            <Topbar />
+            <div className="grid lg:grid-cols-[300px_1fr] grid-cols-[80px_1fr] h-[calc(100vh-180px)]">
+                <UserList />
+            </div>
+        </main>
     );
 };
 
