a51f0b8f613a58ca2a7fd5bdcad47c78ed099b0a
Deni <denislavavmilanova@outlook.com>
2025-04-24 18:03:59 +0300

    feat(backend): add hourly cron job to clean temp files and improve deployment config

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ import { clerkMiddleware } from "@clerk/express";
 import fileupload from "express-fileupload";
 import path from "path";
 import cors from "cors";
+import fs from "fs";
+import cron from "node-cron";
 
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
@@ -42,12 +44,34 @@ app.use(fileupload({
     },
 }));
 
+const tempDir = path.join(process.cwd(), "tmp");
+cron.schedule("0 * * * *", () => {
+    if (fs.existsSync(tempDir)) {
+        fs.readdir(tempDir, (err, files) => {
+            if (err) {
+                console.log("error", err);
+                return;
+            };
+            for (const file of files) {
+                fs.unlink(path.join(tempDir, file), (err) => { });
+            };
+        });
+    };
+});
+
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 app.use((err, req, res, next) => {
     res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal server error" : err.message });
