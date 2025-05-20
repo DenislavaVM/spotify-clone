92b8273e72fb150ba5960ca9123cabedf9f5ae71
Deni <denislavavmilanova@outlook.com>
2025-05-20 11:32:37 +0300

    fix: update frontend build path

--- backend/src/index.js
+++ backend/src/index.js
@@ -79,14 +79,13 @@ app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    const frontendPath = path.join(__dirname, "../frontend/dist");
+    const frontendPath = path.join(__dirname, "../../frontend/dist");
 
-    app.use(express.static(frontendPath));
-
+    app.use(express.static(path.join(__dirname, "..", "frontend", "dist")));
     app.get("*", (req, res) => {
-        res.sendFile(path.join(frontendPath, "index.html"));
+        res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
     });
-}
+};
 
 app.use(errorHandler);
 
